6c27034afbc1fb6ce273d70d59c27674e78dbbd1
Michal Paukert <michal.paukert@radixal.com>
2020-10-28 15:13:27 +0100

    gatsby build fix

--- src/views/contact.tsx
+++ src/views/contact.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 import { ContentLayout } from "../components/Layout/ContentLayout"
+import Articles from "./articles"
 
 export const ContactPage = () => {
   return <ContentLayout title="clanky">Kontakt</ContentLayout>;
 };
+export default ContactPage;
